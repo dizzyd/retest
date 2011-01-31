dfae4fa924e2b55308304119a8cd78d2ea7b4db1
Tuncer Ayaz <tuncer.ayaz@gmail.com>
2011-01-31 12:55:29 +0100

    Update mustache.erl

--- src/mustache.erl
+++ src/mustache.erl
@@ -47,7 +47,7 @@ compile(Mod) ->
 
 compile(Mod, File) ->
   code:purge(Mod),
-  code:load_file(Mod),
+  {module, _} = code:load_file(Mod),
   {ok, TemplateBin} = file:read_file(File),
   Template = re:replace(TemplateBin, "\"", "\\\\\"", [global, {return,list}]),
   State = #mstate{mod = Mod},
@@ -171,13 +171,13 @@ get(Key, Ctx, Mod) ->
     error ->
       case erlang:function_exported(Mod, Key, 1) of
         true ->
-          Val = to_s(apply(Mod, Key, [Ctx])),
+          Val = to_s(Mod:Key(Ctx)),
           % io:format("From Mod/1 {~p, ~p}~n", [Key, Val]),
           Val;
         false ->
           case erlang:function_exported(Mod, Key, 0) of
             true ->
-              Val = to_s(apply(Mod, Key, [])),
+              Val = to_s(Mod:Key()),
               % io:format("From Mod/0 {~p, ~p}~n", [Key, Val]),
               Val;
             false ->
@@ -190,8 +190,6 @@ to_s(Val) when is_integer(Val) ->
   integer_to_list(Val);
 to_s(Val) when is_float(Val) ->
   io_lib:format("~.2f", [Val]);
-to_s(Val) when is_boolean(Val) ->
-  Val;
 to_s(Val) when is_atom(Val) ->
   atom_to_list(Val);
 to_s(Val) ->
